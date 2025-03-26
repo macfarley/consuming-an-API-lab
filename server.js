b1d51303c583f3f0e9f2b338c163cefced33e711
macfarley90 <therealmccoyster@gmail.com>
2025-03-26 19:57:07 -0400

    worked on this during support time, trying to implement bonus features like a  day forecast.

--- server.js
+++ server.js
@@ -39,21 +39,56 @@ submitButton.addEventListener("click", (event) => {
     });
 });
 app.post("/weather", async (req, res) => {
-  const zipCode = req.body.zipCode;
+  const zipCode = req.body.ZipCode;
   const apiKey = process.env.API_KEY;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&units=imperial&appid=${apiKey}`;
-
+  const fiveDayApiUrl = `https://api.openweathermap.org/data/2.5/forecast?zip=${zipCode},us&appid=${apiKey}`;
   try {
     const response = await fetch(apiUrl);
+    const forecastResponse = await fetch(fiveDayApiUrl);
+    const forecastData = await forecastResponse.json();
     const weatherData = await response.json();
-    res.render("./weather/show.ejs", { weather: weatherData });
-    // console.log(weather);
+    
+    function convertTimestampToDate(timestampObject) {
+      const date = new Date(timestampObject * 1000);
+      return date;
+      }
+
+    // for each date in the forecast object, i want a Daily high temperature, and if it will rain, be cloudy, or clear
+    const timeArray = forecastData.list.map(timestamp => {
+      return convertTimestampToDate(timestamp.dt);
+    });
+    const daysInArray = []
+    const weatherDataByDay = []
+    for(let i = 0; i< forecastData.list.length; i++){
+      //extract month and day from a date to make a key MM-DD
+      const date = convertTimestampToDate(forecastData.list[i].dt);
+
+      //if that key is not inside daysInArray, push in
+          //create a new object with forecastData
+          {
+            "date":"YYYY-MM-DD",
+            "weather":"cloudy",
+            "high":283,
+            "average":270,
+            "dayOfWeek":"Monday"
+            }
+          //push the key into daysInArray
+          //push data object into weatherDataByDay
+    }
+  // {day of the week: Thursday,
+  //   date: mm/dd/yyyy,
+  //   high temperature: 001 F,
+  //   weather: description,
+  // }
+
+    res.render("./weather/show.ejs", { weather: weatherData, times: timeArray });
   } catch (error) {
     console.error("Invalid Zip Code", error);
     res.status(500).send("Error retrieving weather data");
   }
 });
-
+// five day forecast by city name
 
 //when the server is running
 const port = process.env.PORT || 3000;
